be23c26bc80a88beef277badc75ce71dc2dcae87
matubarashinya <is2baseball1130@icloud.com>
2021-04-16 19:44:33 +0900

    とりあえずコミット

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // 課題①-1
 
-for (var i = 1; i <= 100; i++) {
-  if (i % 15 === 0) {
-    console.log("FizzBuzz",i);
-  }
-  else if (i % 3 === 0){
-    console.log("Fizz",i);
-  }
-  else if(i % 5 === 0){
-    console.log("Buzz",i);
-  }
-}
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 15 === 0) {
+//     console.log("FizzBuzz",i);
+//   }
+//   else if (i % 3 === 0){
+//     console.log("Fizz",i);
+//   }
+//   else if(i % 5 === 0){
+//     console.log("Buzz",i);
+//   }
+// }
+
+// 課題①-3
+
