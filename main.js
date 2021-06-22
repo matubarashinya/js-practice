b647f67da7073e79f5b1b1084dad45b539650bb8
matubarashinya <is2baseball1130@icloud.com>
2021-06-22 17:25:11 +0900

    fizzbuzz問題のプルリクエスト 改修

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 document.getElementById("form-button").onclick = function() {
 
-  let f_number = document.getElementById("fizznumber").value;
-  let b_number = document.getElementById("buzznumber").value;
-  let parent = document.getElementById('answer');
+  const fuzzNumber = document.getElementById("fizznumber").value;
+  const buzzNumber = document.getElementById("buzznumber").value;
+  const addElement = document.getElementById('answer');
  
 
-  if(isNaN(f_number) || isNaN(b_number)) {
-    var e = document.createElement("p");
+  if(isNaN(fuzzNumber) || isNaN(buzzNumber)) {
+    const e = document.createElement("p");
     e.innerHTML = "整数値を入力してください";
-    parent.appendChild(e);
+    addElement.appendChild(e);
   } else {
-    for (var i = 1; i < 99; i++) {
-      var e = document.createElement("div");
-      if (i % f_number === 0 && i % b_number === 0) {
-        e.innerText = "FizzBuzz" + " " + i ;
-        parent.appendChild(e);
-      }
-      else if (i % f_number === 0){
-        e.innerText = "Fizz" + " " + i ;
-        parent.appendChild(e);
-      }
-      else if(i % b_number === 0){
-        e.innerText = "Buzz" + " " + i ;
-        parent.appendChild(e);
+    for (const i = 1; i < 99; i++) {
+      const v = document.createElement("div");
+      if (i % fuzzNumber === 0 && i % buzzNumber === 0) {
+        v.innerText = "FizzBuzz" + " " + i ;
+        addElement.appendChild(v);
+      } else if (i % fuzzNumber === 0){
+        v.innerText = "Fizz" + " " + i ;
+        addElement.appendChild(v);
+      } else if(i % buzzNumber === 0){
+        v.innerText = "Buzz" + " " + i ;
+        addElement.appendChild(v);
       }
     }
   }
