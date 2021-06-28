b96310343a4eb28dce82e11259b4f3a8c63f03a1
matubarashinya <is2baseball1130@icloud.com>
2021-06-28 22:21:21 +0900

    todoリスト 追加機能

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-document.getElementById("form-button").onclick = function() {
+let i = 0;
 
-  const fuzzNumber = document.getElementById("fizznumber").value;
-  const buzzNumber = document.getElementById("buzznumber").value;
-  const addElement = document.getElementById('answer');
+document.getElementById("addTask").onclick = function() {
+
+  const taskContent = document.getElementById("task_content").value;
+  const statusDisplay = document.getElementById("status_display");
  
+  if(taskContent) {
 
-  if(isNaN(fuzzNumber) || isNaN(buzzNumber)) {
     const e = document.createElement("p");
-    e.innerHTML = "整数値を入力してください";
-    addElement.appendChild(e);
-  } else {
-    for (const i = 1; i < 99; i++) {
-      const v = document.createElement("div");
-      if (i % fuzzNumber === 0 && i % buzzNumber === 0) {
-        v.innerText = "FizzBuzz" + " " + i ;
-        addElement.appendChild(v);
-      } else if (i % fuzzNumber === 0){
-        v.innerText = "Fizz" + " " + i ;
-        addElement.appendChild(v);
-      } else if(i % buzzNumber === 0){
-        v.innerText = "Buzz" + " " + i ;
-        addElement.appendChild(v);
-      }
-    }
-  }
+    e.innerHTML = i + " " + taskContent + " " + '<button type="button">作業中</button>' + " " + '<button type="button">削除</button>';
+    statusDisplay.appendChild(e);
+    i++ ;
+  };
 }
-
